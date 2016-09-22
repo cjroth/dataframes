855d426755cc83d9bee07542280ed4b073c760ec
Chris Roth <chris@cjroth.com>
2016-09-22 22:08:50 +0900

    more

---
+++ TODO.md
@@ -0,0 +1,29 @@
+# To Do
+
+- reverse columns/rows
+- rotate (switch rows and columns)
+
+- min
+- max
+- groupBy
+- movingAverage (simple)
+- movingAverage (exponential)
+- mode
+- product
+
+- column names
+
+- look into integrating with existing stats library
+
+- fromCSV
+- toCSV
+- fromJSON
+- toJSON
+
+- type casting of columns
+
+
+# Long Term
+
+- native d3 support
+- native support of times (moment, moment-range, moment-timezone)
--- index.js
+++ index.js
@@ -4,31 +4,55 @@ const DataFrame = require('./lib/DataFrame')
 const v = Vector.of
 const df = DataFrame.of
 
-let df1 = df(
-    v(1, 2, 3),
-    v(4, 5, 6),
-    v(7, 8, 9)
-)
-// 1 2 3
-// 4 5 6
-// 7 8 9
-console.log(df1)
-console.log(`width: ${df1.width}`, `height: ${df1.height}`)
+// let df1 = df(
+//     v(1, 2, 3),
+//     v(4, 5, 6),
+//     v(7, 8, 9)
+// )
+// // 1 2 3
+// // 4 5 6
+// // 7 8 9
+// console.log(df1)
+// console.log(`width: ${df1.width}`, `height: ${df1.height}`)
+//
+// let where = df1.where(x => x[1] == 5)
+// // [4, 5, 6]
+// console.log('where', where)
+//
+// let column = df1.column(1)
+// // [2, 5, 8]
+// console.log(column)
+//
+// let column2 = df1[2]
+// console.log(column2)
+//
+// let row = df1.row(2)
+// // [7, 8, 9]
+// console.log(row)
+//
+// let mean = df1.sum()
+// console.log(mean)
 
-let where = df1.where(x => x[1] == 5)
-// [4, 5, 6]
-console.log('where', where)
+// let a = v(
+//     v(1, 2, 3),
+//     v(4, 5, 6),
+//     v(7, 8, 9)
+// ).mean()
+//
+// let b = v(1, 2, 3).sum()
 
-let column = df1.column(1)
-// [2, 5, 8]
-console.log(column)
+const fs = require('fs')
+let chris = JSON.parse(fs.readFileSync('../lifeslice/emotioncrunch/results/chris.json'))
+// .map(m => {
+//     return {
+//         valence: m.emotions.valence,
+//         joy: m.emotions.joy,
+//
+//     }
+// })
 
-let column2 = df1[2]
-console.log(column2)
+console.log(chris)
 
-let row = df1.row(2)
-// [7, 8, 9]
-console.log(row)
+let c = Vector.fromJSON(chris)
 
-let mean = df1.sum()
-console.log(mean)
+console.log(c.column(0).mean())
